398329e7ed745a6fc0481cce27a6186e0270ddcd
DipanshuChawde <dipanshuchoude@gmail.com>
2024-11-22 21:13:44 +0530

    22 nov

--- cypress/e2e/3-basics/2.contactUs.cy.js
+++ cypress/e2e/3-basics/2.contactUs.cy.js
@@ -1,7 +1,7 @@
 ///<reference types='cypress' />
 
-describe('verify contact us page',function(){
-    it('verify contact us page for valid data',function(){
+describe('TS001 : verify contact us page',function(){ //suite 
+    it('TC001 - verify contact us page for valid data',function(){ //test case1
         //AAA
 
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
@@ -14,5 +14,50 @@ describe('verify contact us page',function(){
         cy.get('[type="submit"]').click()
 
         cy.get('h1').should('have.text','Thank You for your Message!')
+        
+    })
+
+    it('varify contact us page for invalid emailid', function(){
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+
+        cy.get('[name="first_name"]').type('Neel')
+        cy.get('[name="last_name"]').type('chawde')
+        cy.get('[name="email"]').type('neel')
+        cy.get('[name="message"]').type('hi')
+
+        cy.get('[type="submit"]').click()
+
+        cy.contains(' Error: Invalid email address').should('be.visible')
+    })
+
+    it.only('varify contact us page for skip field', function(){
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+
+        cy.get('[name="first_name"]').type('Neel')
+        
+        cy.get('[name="email"]').type('neelchawde@gmail.com')
+        cy.get('[name="message"]').type('hi')
+
+        cy.get('[type="submit"]').click()
+
+        cy.contains(' Error: all fields are required').should('be.visible')
+    })
+
+    it.only('varify contact us page for reset button', function(){
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+
+        cy.get('[name="first_name"]').type('Neel')
+        cy.get('[name="last_name"]').type('chawde')
+        cy.get('[name="email"]').type('neel')
+        cy.get('[name="message"]').type('hi')
+
+        cy.get('[type="reset"]').click()
+
+        cy.get('[name="first_name"]').should('have.text',"")
+        cy.get('[name="last_name"]').should('have.text',"")
+        cy.get('[name="email"]').should('have.text',"")
+        cy.get('[name="message"]').should('have.text',"")
+
+
     })
 })
