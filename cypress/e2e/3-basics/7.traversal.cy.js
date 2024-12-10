2c4044b870a049d17e585d0e7d99ed1727b6f92e
DipanshuChawde <dipanshuchoude@gmail.com>
2024-12-10 08:43:11 +0530

    9 dec

---
+++ cypress/e2e/3-basics/7.traversal.cy.js
@@ -0,0 +1,27 @@
+/// <reference types ="cypress" />
+
+describe('traversing DOM element', () => {
+
+    beforeEach(function () {
+        cy.visit('https://www.webdriveruniversity.com/Data-Table/index.html')
+    })
+
+    it('Tarvarsing1', () => {
+        cy.get('.traversal-drinks-list >li').should('have.length',5)
+        cy.get('.traversal-drinks-list >li').first().should('have.text', 'Coffee')
+        cy.get('.traversal-drinks-list >li').last().should('have.text', 'Sugar')
+        cy.get('.traversal-drinks-list >li').eq(2).should('have.text', 'Milk')
+        cy.get('.traversal-drinks-list >li:nth-child(1)').should('have.text', 'Coffee')
+        cy.get('.traversal-drinks-list >li:nth-child(5)').should('have.text', 'Sugar')
+        cy.get('.traversal-drinks-list >li:nth-child(3)').should('have.text', 'Milk')
+    })
+
+    it.only('siblings',()=>{
+        cy.get('#veggie').prev().should('have.text','Figs')
+        cy.get('#veggie').next().should('have.text','Asparagus')
+        cy.get('#veggie').prevAll().should('have.length',6)
+        cy.get('#veggie').nextAll().should('have.length',4)
+        cy.get('#espresso').prevUntil('#coffee').should('have.length',2)
+    })
+
+})
