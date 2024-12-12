51c5cc4feb8bc5e209fff36c3e333d3075794b3f
DipanshuChawde <dipanshuchoude@gmail.com>
2024-12-12 22:05:25 +0530

    12 dec

--- cypress/e2e/3-basics/8.traverseFlipkart.cy.js
+++ cypress/e2e/3-basics/8.traverseFlipkart.cy.js
@@ -7,5 +7,6 @@ describe('traversing DOM element', () => {
         cy.get('[placeholder="Search for Products, Brands and More"]').type('bag {enter}')
         //cy.get('#container > div > div.nt6sNV.JxFEK3._48O0EI > div.DOjaWF.YJG4Cf > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(1) > div > div > div.syl9yP')
         cy.get('div[class="syl9yP"]').first().should('have.text', 'SBCOLLECTION')
+        cy.get('div[class="syl9yP"]:first').should('have.text', 'SBCOLLECTION')
     })
 })
