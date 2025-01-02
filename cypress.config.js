60f0f86304bd74409e4ca8fe41f9c4dc69a7e6b8
DipanshuChawde <dipanshuchoude@gmail.com>
2025-01-02 22:08:09 +0530

    2jan25

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   //includeShadowDom : true,
+  chromeWebSecurity : false, //for multi tab or multi-window
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
