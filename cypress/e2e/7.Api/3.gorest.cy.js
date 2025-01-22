b8de915853d5186281f634c6850369357ed4d49a
DipanshuChawde <dipanshuchoude@gmail.com>
2025-01-22 21:58:37 +0530

    22jan25

---
+++ cypress/e2e/7.Api/3.gorest.cy.js
@@ -0,0 +1,27 @@
+///<reference types='cypress' />
+import postD from "../../fixtures/1.Api/gorPostData"
+
+describe('TS001 : verify reqres Api for gorest', function () {
+    postD.forEach((el, index) => {
+        it('TC001 - verify reqres Api-post Get put delete method', function () {
+            const token = '2e5d6a76c6c9f78db8669486a0bc356d8c5acaa7a0adac87796e8eaef1619f8e'
+            cy.request({
+                url: 'https://gorest.co.in/public/v2/users',
+                method: 'POST',
+                body: el,
+                // {
+                //     "name": "Neel Chawde",
+                //     "gender": "male",
+                //     "email": "neel133@gmail.com",
+                //     "status": "active"
+                // },
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((respost) => {
+                cy.log(respost.body.id)
+                expect(respost.body.name).to.eq(el.name)
+            })
+        })
+    })
+})
