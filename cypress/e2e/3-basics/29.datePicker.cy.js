47048c7543dbde7644a5d6b20b907ecb1fff8437
DipanshuChawde <dipanshuchoude@gmail.com>
2025-01-31 21:34:31 +0530

    30jan25

--- cypress/e2e/3-basics/29.datePicker.cy.js
+++ cypress/e2e/3-basics/29.datePicker.cy.js
@@ -1,68 +1,93 @@
 ///<reference types = "cypress" /> 
-describe("verify sate picker functionality in cypress",()=>{
-    it("verify date picker for current and future date",()=>{
+describe("verify sate picker functionality in cypress", () => {
+    it("verify date picker for current and future date", () => {
         let currentDate = new Date()
         cy.log(`current date is  ${currentDate}`)
 
-        let todayDate =currentDate.getDate()
+        let todayDate = currentDate.getDate()
         cy.log(`current date is ${todayDate}`)
 
         let todayMonth = currentDate.getMonth()  //starts with 0 as jan
         cy.log(`current month is ${todayMonth + 1} `)
 
-        let todayMonthNSh = currentDate.toLocaleString("default",{month : "short"})
+        let todayMonthNSh = currentDate.toLocaleString("default", { month: "short" })
         cy.log(`current month is ${todayMonthNSh} `)
 
-        let todayMonthLong = currentDate.toLocaleString("default",{month : "long"})
+        let todayMonthLong = currentDate.toLocaleString("default", { month: "long" })
         cy.log(`current month is ${todayMonthLong} `)
 
         //future date
         let dateF = new Date()
 
-        dateF.setDate(dateF.getDate()+31)
+        dateF.setDate(dateF.getDate() + 31)
         cy.log(`future date is ${dateF}`)
 
-        let fDate =dateF.getDate()
+        let fDate = dateF.getDate()
         cy.log(`current date is ${fDate}`)
 
         let fMonth = dateF.getMonth()  //starts with 0 as jan
         cy.log(`current month is ${fMonth + 1} `)
 
-        let fMonthNSh = dateF.toLocaleString("default",{month : "short"})
+        let fMonthNSh = dateF.toLocaleString("default", { month: "short" })
         cy.log(`current month is ${fMonthNSh} `)
 
-        let fMonthLong = dateF.toLocaleString("default",{month : "long"})
+        let fMonthLong = dateF.toLocaleString("default", { month: "long" })
         cy.log(`current month is ${fMonthLong} `)
     })
 
-    it.only('verify future date functionality',()=>{
+    it.only('verify future date functionality', () => {
 
-         //future date
-         let dateF = new Date()
+        //future date
+        let dateF = new Date()
 
-         dateF.setDate(dateF.getDate()+396) 
-         cy.log(`future date is ${dateF}`)
- 
-         let fDate =dateF.getDate()
-         cy.log(`future date is ${fDate}`)
- 
-         let fMonth = dateF.getMonth()  //starts with 0 as jan
-         cy.log(`future month is ${fMonth + 1} `)
- 
-         let fMonthNSh = dateF.toLocaleString("default",{month : "short"})
-         cy.log(`future month is ${fMonthNSh} `)
- 
-         let fMonthLong = dateF.toLocaleString("default",{month : "long"})
-         cy.log(`future month is ${fMonthLong} `)
+        dateF.setDate(dateF.getDate() + 396)
+        cy.log(`future date is ${dateF}`)
 
-         let fyear = dateF.getFullYear()
-         cy.log(`future year is ${fyear} `)
+        let fDate = dateF.getDate()
+        cy.log(`future date is ${fDate}`)
+
+        let fMonth = dateF.getMonth()  //starts with 0 as jan
+        cy.log(`future month is ${fMonth + 1} `)
+
+        let fMonthNSh = dateF.toLocaleString("default", { month: "short" })
+        cy.log(`future month is ${fMonthNSh} `)
+
+        let fMonthLong = dateF.toLocaleString("default", { month: "long" })
+        cy.log(`future month is ${fMonthLong} `)
+
+        let fyear = dateF.getFullYear()
+        cy.log(`future year is ${fyear} `)
 
         cy.visit('https://www.webdriveruniversity.com/Datepicker/index.html')
         cy.get('.input-group-addon').click()
 
-        
+        function selectYearMonth() {
 
+           
+            cy.get('.datepicker-switch').first().then(function(currentDate) {
+                //cy.log(currentDate.text())
+                if(!currentDate.text().includes(dateF.getFullYear())) {
+                    cy.get('.next').first().click()
+                    selectYearMonth()
+                }
+            }).then(function(){
+                cy.get('.datepicker-switch').first().then(function(currentDate){
+                    if(!currentDate.text().includes(fMonthLong)) {
+                        cy.get('.next').first().click()
+                        selectYearMonth()
+                    }
+                })
+            })
+        }
+
+        function selectDate(){
+            cy.get('[class="day"]').contains(fDate).click()
+           
+        }
+
+
+        selectYearMonth()
+        selectDate()
 
     })
 })
