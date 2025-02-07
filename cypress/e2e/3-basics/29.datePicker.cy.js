738094142fe82b716935382d9b7dca5cbdb726d6
DipanshuChawde <dipanshuchoude@gmail.com>
2025-02-07 22:27:37 +0530

    07feb25

--- cypress/e2e/3-basics/29.datePicker.cy.js
+++ cypress/e2e/3-basics/29.datePicker.cy.js
@@ -35,7 +35,7 @@ describe("verify sate picker functionality in cypress", () => {
         cy.log(`current month is ${fMonthLong} `)
     })
 
-    it.only('verify future date functionality', () => {
+    it('verify future date functionality', () => {
 
         //future date
         let dateF = new Date()
@@ -63,16 +63,16 @@ describe("verify sate picker functionality in cypress", () => {
 
         function selectYearMonth() {
 
-           
-            cy.get('.datepicker-switch').first().then(function(currentDate) {
+
+            cy.get('.datepicker-switch').first().then(function (currentDate) {
                 //cy.log(currentDate.text())
-                if(!currentDate.text().includes(dateF.getFullYear())) {
+                if (!currentDate.text().includes(dateF.getFullYear())) {
                     cy.get('.next').first().click()
                     selectYearMonth()
                 }
-            }).then(function(){
-                cy.get('.datepicker-switch').first().then(function(currentDate){
-                    if(!currentDate.text().includes(fMonthLong)) {
+            }).then(function () {
+                cy.get('.datepicker-switch').first().then(function (currentDate) {
+                    if (!currentDate.text().includes(fMonthLong)) {
                         cy.get('.next').first().click()
                         selectYearMonth()
                     }
@@ -80,9 +80,9 @@ describe("verify sate picker functionality in cypress", () => {
             })
         }
 
-        function selectDate(){
+        function selectDate() {
             cy.get('[class="day"]').contains(fDate).click()
-           
+
         }
 
 
@@ -90,4 +90,50 @@ describe("verify sate picker functionality in cypress", () => {
         selectDate()
 
     })
+
+    // it.only('TC002 verify past date picker', function () {
+    //     let Cdate = new Date()
+    //     // cy.log(current date:${Cdate})
+
+    //     Cdate.setDate(Cdate.getDate() - 1000)
+    //     cy.log(`get past date:${Cdate}`)
+
+    //     let pdate = Cdate.getDate()
+    //     cy.log(`get past date:${pdate}`)
+
+    //     let pMonth = Cdate.toLocaleString("default", { month: "long" })
+    //     cy.log(`get past month:${pMonth}`)
+
+    //     let pYear = Cdate.getFullYear()
+    //     cy.log(`get past year:${pYear}`)
+
+    //     cy.visit("https://www.webdriveruniversity.com/Datepicker/index.html")
+
+    //     cy.get('.form-control').click()
+
+    //     function selectYearMonth() {
+    //         cy.get('.datepicker-switch').first().then(function (Cdate) {
+    //             // cy.log(Tdate.text())
+    //             if (!Cdate.text().includes(pYear)) {
+    //                 cy.get('.prev').first().click()
+    //                 selectYearMonth()
+    //             }
+
+    //         }).then(function () {
+    //             cy.get('.datepicker-switch').first().then((Cdate) => {
+    //                 if (!Cdate.text().includes(pMonth)) {
+    //                     cy.get('.prev').first().click()
+    //                     selectYearMonth()
+    //                 }
+    //             })
+    //         })
+    //     }
+
+    //     function selectDate() {
+    //         cy.get("[class='day']").contains(pdate).click()
+    //     }
+    //     selectYearMonth()
+    //     selectDate()
+    //     cy.get('.form-control').should('have.value', '05-13-2022')
+    // })
 })
--- cypress/e2e/8.POM/pages/1.page1.js
+++ cypress/e2e/8.POM/pages/1.page1.js
@@ -23,6 +23,27 @@ export default class myPage {
         cy.get(this.selector.msg).type("i m learning pom")
     }
 
+    contactUsPage2(fname,lname,mailId,mg){
+        cy.get(this.selector.fn).type(fname)
+        cy.get(this.selector.ln).type(lname)
+        cy.get(this.selector.email).type(mailId)
+        cy.get(this.selector.msg).type(mg)
+    }
+    contactUsPage3 (user){
+        cy.get(this.selector.fn).type(user.name)
+        cy.get(this.selector.ln).type(user.lastname)
+        cy.get(this.selector.email).type(user.email)
+        cy.get(this.selector.msg).type(user.message)
+    }
+
+    contactUsPage4 (element){
+        cy.get(this.selector.fn).type(element.fn)
+        cy.get(this.selector.ln).type(element.ln)
+        cy.get(this.selector.email).type(element.email)
+        cy.get(this.selector.msg).type(element.msg)
+    }
+    
+
     btnClick() {
         cy.get(this.selector.submitBtn).click()
     }
