1270872b094674009a7c41005c324dc467cbfde8
DipanshuChawde <dipanshuchoude@gmail.com>
2025-02-13 22:09:54 +0530

    13feb25

--- cypress/e2e/8.POM/pages/2.page2.js
+++ cypress/e2e/8.POM/pages/2.page2.js
@@ -1,32 +1,49 @@
 export default class homePage {
     selectors = {
+       // main page
         url: "https://automationexercise.com/",
         loginSignUpbtn: 'a[href="/login"]',
         // newUserForm : 'form[action="/signup"] ',
+        //new user login page signup
         newUserName: '[data-qa="signup-name"]',
         newUserEmail: '[data-qa="signup-email"]',
         signUpBtn: '[data-qa="signup-button"]',
-       MrBtn : '[id="id_gender1"]',
-       MrsBtn : '[id ="id_gender2"]',
-       passwordCss : '[data-qa="password"]',
-       dayCss : '[data-qa="days"]',
-       monthCss : '[data-qa="months"]',
-       yearCss : '[data-qa="years"]',
-       newsLtr1 : '[id="newsletter"]',
-       newsLtr2 : '[id="optin"]',
-       firstNameCss : '[data-qa="first_name"]',
-       lastNameCss : '[data-qa="last_name"]',
-       companyCss  : '[for="company"]',
-       address1Css : '[data-qa="address"]',
-       address2Css : '[data-qa="address2"]',
-       countryCss : '[data-qa="country"]',
-       stateCss : '[data-qa="state"]',
-       cityCss : '[data-qa="city"]',
-       zipCodeCss : '[data-qa="zipcode"]',
-       mobNoCss : '[data-qa="mobile_number"]',
-       createAccountBtn : '[data-qa="create-account"]',
-       accountCreatedCss : 'h2 > b',
-       accountCreatedText : 'Account Created!'
+
+         //new user create page
+        MrBtn: '[id="id_gender1"]',
+        MrsBtn: '[id ="id_gender2"]',
+        passwordCss: '[data-qa="password"]',
+        dayCss: '[data-qa="days"]',
+        monthCss: '[data-qa="months"]',
+        yearCss: '[data-qa="years"]',
+        newsLtr1: '[id="newsletter"]',
+        newsLtr2: '[id="optin"]',
+        firstNameCss: '[data-qa="first_name"]',
+        lastNameCss: '[data-qa="last_name"]',
+        companyCss: '[for="company"]',
+        address1Css: '[data-qa="address"]',
+        address2Css: '[data-qa="address2"]',
+        countryCss: '[data-qa="country"]',
+        stateCss: '[data-qa="state"]',
+        cityCss: '[data-qa="city"]',
+        zipCodeCss: '[data-qa="zipcode"]',
+        mobNoCss: '[data-qa="mobile_number"]',
+        createAccountBtn: '[data-qa="create-account"]',
+
+        //new user created page
+        accountCreatedCss: 'h2 > b',
+        accountCreatedText: 'Account Created!',
+        continueBtn : '[data-qa="continue-button"]',
+
+        //loged in page
+        loginNameCss : '[class="nav navbar-nav"] > li >a >b',
+        logoutBtn : 'a[href="/logout"]',
+
+        //existing user login
+        useremailCss : '[data-qa="login-email"]',
+        passwordcsslogin : '[data-qa="login-password"]',
+        loginBtnCss: '[data-qa="login-button"]'
+
     }
 
     urlVisit = (url) => {
@@ -42,8 +59,8 @@ export default class homePage {
         cy.get(this.selectors.newUserName).type(user.NewUNm)
         cy.get(this.selectors.newUserEmail).type(user.NewUEml)
         cy.get(this.selectors.signUpBtn).click()
-        
-        if(user.title =="Mr"){
+
+        if (user.title == "Mr") {
             cy.get(this.selectors.MrBtn).click()
         }
         else {
@@ -66,12 +83,23 @@ export default class homePage {
         cy.get(this.selectors.cityCss).type(user.city)
         cy.get(this.selectors.zipCodeCss).type(user.zipcode)
         cy.get(this.selectors.mobNoCss).type(user.mobNo)
-        this.btnClick(this.selectors.createAccountBtn)  
+        this.btnClick(this.selectors.createAccountBtn)
         cy.wait(2000)
-        this.validationText(this.selectors.accountCreatedCss,this.selectors.accountCreatedText)   
+        //account created page
+        this.validationText(this.selectors.accountCreatedCss, this.selectors.accountCreatedText)
+        this.btnClick(this.selectors.continueBtn)
+        //login home page
+        this.validationText(this.selectors.loginNameCss,user.NewUNm) 
+
     }
 
-    validationText(css,text){
-        cy.get(css).should("have.text",text)
+    validationText(css, text) {
+        cy.get(css).should("have.text", text)
+    }
+
+    loginOrSignUp(nameUerId,passWordEmail,createLogin,inputTextvalue1,inputTextvalue2){
+        cy.get(nameUerId).type(inputTextvalue1)
+        cy.get(passWordEmail).type(inputTextvalue2)
+        this.btnClick(createLogin)
     }
 }
---
+++ cypress/fixtures/PomData/book.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        firstName : "ajay",
+        lastName : "devgan",
+        password : "kajol",
+        userName : function() { return `${this.firstName}_${this.lastName}`; }, 
+        gender  : "male"
+
+    }
+]
